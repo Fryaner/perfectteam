e83622d275531fc66c18c8da60a1d9dcaf7f02e2
Ilya <ilya.orlov123@mail.ru>
2025-03-05 06:13:03 +1000

    fix

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,16 +1,16 @@
 <template>
     <footer class="footer" id="footer">
         <div class="footer__info">
-            <p class="footer__mail"><span>Email: </span>hrfectteam@yandex.ru, hrfectteam@mail.ru</p>
-            <p class="footer__phone"><span>Phone: </span>+7 901 718 34 60 </p>
+            <p class="footer__mail"><span>Email: </span><a href="mailto:hrperfectteam@yandex.ru" aria-label="mail1">hrperfectteam@yandex.ru</a>, <a href="mailto:hrfectteam@mail.ru" aria-label="mail2">hrfectteam@mail.ru</a></p>
+            <p class="footer__phone"><span>Phone: </span><a href="tel:79017183460" aria-label="mail1">+7 901 718 34 60</a> </p>
             <p class="footer__timing">Мы работаем: Пн-Пт, 9:00-18:00</p>
         </div>
         <div class="footer__other">
-            <p class="footer__site">www perfectteam.pro</p>
+            <p class="footer__site"><a target="_blank" href="www.perfectteam.pro">www.perfectteam.pro</a></p>
             <div class="footer__social">
-                <a href="" aria-label="whatsapp"><img src="@/assets/icons/wa.svg" alt=""/></a>
+                <a target="_blank" href="https://wa.me/79017183460" aria-label="whatsapp"><img src="@/assets/icons/wa.svg" alt=""/></a>
                 <a href="" aria-label="vk"><img src="@/assets/icons/vk.svg" alt=""/></a>
-                <a href="" aria-label="tg"><img src="@/assets/icons/tg.svg" alt=""/></a>
+                <a target="_blank" href="https://t.me/PerfectTeam_PT" aria-label="tg"><img src="@/assets/icons/tg.svg" alt=""/></a>
             </div>
             <p class="footer__title">PERFECT TEAM © 2025. All Rights Reserved.</p>
         </div>
@@ -38,6 +38,15 @@
         text-align: center;
         font-size: 24px;
 
+        a {
+            transition: .5s;
+            @media (hover:hover) {
+                &:hover {
+                    color: $color-blue;
+                }
+            }
+        }
+
         span {
             font-weight: 700;
         }
@@ -50,6 +59,15 @@
     &__site {
         font-size: 32px;
         font-weight: 700;
+
+        a {
+            transition: .5s;
+            @media (hover:hover) {
+                &:hover {
+                    color: $color-blue;
+                }
+            }
+        }
 
         @media screen and (max-width: $limit-mobile) {
             font-size: 24px;
